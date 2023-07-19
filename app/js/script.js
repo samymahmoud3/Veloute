9d2e69f4a0ad8bae616d2bc65306644aa5985455
wowtrika22 <samymahmoud2532001@gmail.com>
2023-07-19 23:35:56 +0300

    header js&styles

---
+++ app/js/script.js
@@ -0,0 +1,56 @@
+const hamburger = document.querySelector('#hamburger__menu');
+const header = document.querySelector('.header');
+const header__menu = document.querySelector('.header__menu');
+const fadeElements = document.querySelectorAll('.has-fade'); 
+const body = document.querySelector('body');
+
+hamburger.addEventListener("click", ()=>{
+
+    if(header.classList.contains("open")) {//close hamburger toggle
+        body.classList.remove('noScroll');
+        header.classList.remove('open');
+        
+        fadeElements.forEach((e)=>{
+            e.classList.add('fade-out');
+            e.classList.remove('fade-in');
+        })
+
+    } else { //open hamburger toggle
+        body.classList.add('noScroll');
+        header.classList.add('open');
+        fadeElements.forEach((e)=>{
+            e.classList.add('fade-in');
+            e.classList.remove('fade-out');
+        })
+    }
+})
+
+
+//class active for desktop links
+const header__links = document.querySelectorAll('.header__links a');
+header__links.forEach(function(ele){
+
+    ele.onclick = function(){
+
+        header__links.forEach(function(ele){
+            ele.classList.remove('active');
+        });
+
+        this.classList.add('active');
+    };
+
+});
+//class active for mobile links
+const header_menu = document.querySelectorAll('.header__menu a');
+header_menu.forEach(function(e){
+
+    e.onclick = function() {
+
+        header_menu.forEach(function(e){
+            e.classList.remove('active');
+        });
+
+        this.classList.add('active');
+
+    };
+});
